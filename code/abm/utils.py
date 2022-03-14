53190a658978ab4b84edaa84ffe2c56c7989b38c
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2022-03-14 19:52:57 +0000

    Updated abm

--- code/abm/utils.py
+++ code/abm/utils.py
@@ -1,5 +1,4 @@
 
-
 class Agent():
     def __init__(self, id, t, θ, B, rule):
         self.id = id
@@ -24,13 +23,16 @@ class Agent():
     def update(self, ai, aj, partner, t):
         if ai and aj:
             self.matches.append((partner.id, partner.θ))
-            self.observed_ρ = len(self.matches)/(t-self.birth)
+            self.observed_ρ = len(self.matches)/(t-self.birth+1)
 
         elif aj:
             self.rs_received.append((partner.id, partner.birth, partner.θ))
-            self.true_ρ = len(self.rs_received)/(t-self.birth) 
+            self.true_ρ = len(self.rs_received)/(t-self.birth+1) 
 
-    def kill(self, t):
-        self.death = t
+def kill(agent, platform, t):
+    agent.death = t
+    platform.remove(agent)
+
+
         
 
