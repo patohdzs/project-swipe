e6fc8a5bfc2b274eb26473223aca5d44dde85b2a
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2022-03-21 16:43:42 -0400

    updated abm
    - added mass convergance plots

--- code/abm/utils.py
+++ code/abm/utils.py
@@ -1,8 +1,8 @@
-
 class Agent():
-    def __init__(self, id, t, θ, B, rule):
+    def __init__(self, id, t, sex, θ, B, rule):
         self.id = id
         self.birth = t
+        self.sex = sex
         self.θ = θ
         self.b = B 
         self.observed_ρ = 1
@@ -15,19 +15,31 @@ class Agent():
     def swipe(self, partner):
         if partner.θ > self.rule[self.b - 1]:
             self.b = self.b - 1
-            self.rs_given.append((partner.id, partner.θ))
+            self.rs_given.append(partner.id)
             return True
         return False    
-
-
+        
     def update(self, ai, aj, partner, t):
         if ai and aj:
-            self.matches.append((partner.id, partner.θ))
+            self.matches.append(partner.id)
             self.observed_ρ = len(self.matches)/(t-self.birth+1)
 
-        elif aj:
-            self.rs_received.append((partner.id, partner.birth, partner.θ))
+        if aj:
+            self.rs_received.append(partner.id)
             self.true_ρ = len(self.rs_received)/(t-self.birth+1) 
+
+    def info(self, t):
+        return {'id':self.id,
+                'time':t, 
+                'birth':self.birth,
+                'sex':self.sex, 
+                'attractiveness':self.θ, 
+                'budget':self.b, 
+                'observed_rate':self.observed_ρ, 
+                'true_rate':self.true_ρ,  
+                'matches':self.matches.copy(), 
+                'rs_given':self.rs_given.copy(), 
+                'rs_received':self.rs_received.copy()}
 
 def kill(agent, platform, t):
     agent.death = t
