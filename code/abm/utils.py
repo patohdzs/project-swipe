39b889dc3b0472228d08647e5160370ece2e75c6
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2022-03-25 21:02:22 -0400

    Updated model 2
    - big change: equilibrium with different distributions solved

--- code/abm/utils.py
+++ code/abm/utils.py
@@ -8,38 +8,41 @@ class Agent():
         self.observed_ρ = 1
         self.true_ρ = 1
         self.rule = rule
-        self.matches = []
-        self.rs_given = [] # ids of agents that were given rs
-        self.rs_received = [] # ids of agents who gave rs
+        self.matches = 0
+        self.rs_given = 0 # ids of agents that were given rs
+        self.rs_received = 0 # ids of agents who gave rs
 
-    def swipe(self, partner):
-        if partner.θ > self.rule[self.b - 1]:
-            self.b = self.b - 1
-            self.rs_given.append(partner.id)
+    def swipe(self, partner, t):
+        if (partner.θ > self.rule[self.b - 1]):
+            self.b -= 1
+            if (self.b<0):
+                print(f'ouch... something wrong with {self.sex} {self.id} @t={t}')
+            self.rs_given += 1
             return True
         return False    
         
-    def update(self, ai, aj, partner, t):
+    def update(self, ai, aj, t):
         if ai and aj:
-            self.matches.append(partner.id)
-            self.observed_ρ = len(self.matches)/(t-self.birth+1)
+            self.matches += 1
+            self.observed_ρ = self.matches/(t-self.birth+1)
 
         if aj:
-            self.rs_received.append(partner.id)
-            self.true_ρ = len(self.rs_received)/(t-self.birth+1) 
+            self.rs_received += 1
+            self.true_ρ = self.rs_received/(t-self.birth+1) 
 
-    def info(self, t):
-        return {'id':self.id,
-                'time':t, 
-                'birth':self.birth,
-                'sex':self.sex, 
-                'attractiveness':self.θ, 
-                'budget':self.b, 
-                'observed_rate':self.observed_ρ, 
-                'true_rate':self.true_ρ,  
-                'matches':self.matches.copy(), 
-                'rs_given':self.rs_given.copy(), 
-                'rs_received':self.rs_received.copy()}
+    def info(self, batch, t):
+        return {'batch' : batch,
+                'id' : self.id,
+                'time' : t, 
+                'birth' : self.birth,
+                'sex' : self.sex, 
+                'attractiveness' : self.θ, 
+                'budget' : self.b, 
+                'observed_rate' : self.observed_ρ, 
+                'true_rate' : self.true_ρ,  
+                'matches' : self.matches, 
+                'rs_given' : self.rs_given, 
+                'rs_received' : self.rs_received}
 
 def kill(agent, platform, t):
     agent.death = t
